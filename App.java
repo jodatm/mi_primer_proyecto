874f2e4e583348df8cfa7d9c367da0eabb6be3fb
jodatm <joshuadtm@gmail.com>
2024-08-29 11:12:21 -0500

    agregar un mensaje de ayuda

--- App.java
+++ App.java
@@ -5,6 +5,7 @@ public class App{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         byte edad = scanner.nextByte();
+        System.out.println("Ingresa tu edad");
         if(edad<18){
             System.out.println("menor de edad");
         }else{
